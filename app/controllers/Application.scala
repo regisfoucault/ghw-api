26696dd0f76bf74a74a49d92e5245bddf268342e
regisfoucault <regisfoucault@gmail.com>
2012-11-12 15:09:19 +0100

    First commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.ws.WS
+
+object Application extends Controller {
+
+  def searchRepo(keyword: String) = Action {
+    val feedUrl = "https://api.github.com/legacy/repos/search/" + keyword
+    Async {
+      WS.url(feedUrl).get().map { response =>
+        Ok(response.json)
+      }
+    }
+  }
+
+  def contributors(owner: String, repo: String) = Action {
+    val feedUrl = "https://api.github.com/repos/" + owner + "/" + repo + "/contributors"
+    Async {
+      WS.url(feedUrl).get().map { response =>
+        Ok(response.json)
+      }
+    }
+  }
+
+  def commits(owner: String, repo: String) = Action {
+    val feedUrl = "https://api.github.com/repos/" + owner + "/" + repo + "/commits?per_page=100"
+    Async {
+      WS.url(feedUrl).get().map { response =>
+        Ok(response.json)
+      }
+    }
+  }
+  
+}
